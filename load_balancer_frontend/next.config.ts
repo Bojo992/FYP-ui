3bd5efceacf64205943ff50348b2fa6c29221191
Bojo992 <mm22mm44bbi@gmail.com>
2025-04-05 20:02:45 +0100

    SFDsfasd

--- load_balancer_frontend/next.config.ts
+++ load_balancer_frontend/next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    basePath: "/config",
-    output: "export"
-};
+const env = process.env.NODE_ENV || "development";
+let temp: NextConfig;
+
+if (env === "development") {
+    temp = {
+        basePath: "/config",
+        assetPrefix: "/config",
+        output: "export"
+    };
+} else {
+    temp = {
+        basePath: "/FYP-ui/config",
+        assetPrefix: "/FYP-ui/config",
+        output: "export"
+    };
+}
+
+const nextConfig: NextConfig = temp;
 
 export default nextConfig;
